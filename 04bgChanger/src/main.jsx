51db3134c027c5501752c0c8282de3a99f8dad4a
Veeresh Prajapati <veereshk.prajapati456@gmail.com>
2024-12-28 01:55:27 +0530

    Done here

--- 04bgChanger/src/main.jsx
+++ 04bgChanger/src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import SmartBulb from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SmartBulb />
   </StrictMode>,
 )
